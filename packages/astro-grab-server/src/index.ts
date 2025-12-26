7d5a7c25bb46411d0316e983dd628042c448677c
Nick Radford <nick@nicholasradford.com>
2025-12-26 01:59:10 -0800

    feat: enable snippet extraction in production demo
    Add build-time source embedding to enable astro-grab snippet extraction
    in serverless production environments for the demo deployment.

--- packages/astro-grab-server/src/index.ts
+++ packages/astro-grab-server/src/index.ts
@@ -3,3 +3,5 @@ export type { AstroGrabPluginOptions } from "./vite-plugin.js";
 export { instrumentAstroFile } from "./parser.js";
 export { transformAstroFile } from "./transform.js";
 export { handleSnippetRequest } from "./snippet-handler.js";
+export { handleSnippetRequestFromContent } from "./snippet-handler-content.js";
+export type { SnippetHandlerOptions } from "./snippet-handler-content.js";
