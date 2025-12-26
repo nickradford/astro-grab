5980d4cff9bf095131ef2210f2182104adc5bd09
Nick Radford <nick@nicholasradford.com>
2025-12-26 00:54:19 -0800

    vercel setup

--- apps/demo/astro.config.mjs
+++ apps/demo/astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config";
 import { astroGrab } from "astro-grab";
 
 import tailwindcss from "@tailwindcss/vite";
+import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [astroGrab()],
-
+  output: "server",
+  adapter: vercelServerless(),
   vite: {
     plugins: [tailwindcss()],
   },
